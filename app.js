8db0cd8fba03e1054f03ace254a416bbc8a7fc7c
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-04-23 14:27:52 +0300

    static middleware and middleware concept

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//örnek bir middleware yazalım
+const myLogger = (req,res, next) =>{  
+      console.log("Middleware Log 1");
+      next(); //middleware takılı kalmasın diye devam ettiriyoruz
+}
+app.use(myLogger); //middleware ı çağıralım
+
+
+
+
+// middle wares
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
