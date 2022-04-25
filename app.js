606fb04ff9586046db89e0fc9c55934d7f127962
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-04-25 16:01:13 +0300

    formdan görsel çekmek ve bunu kaydetmek
    bu çalışmamızda form üzerinden expressfile upload ile görsel yüklemeyi öğrendik

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
-const Photo = require('./models/Photo')
-
+const fs = require('fs');
+const Photo = require('./models/Photo');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
-
 //database bağlantısı
-mongoose.connect('mongodb://localhost/pcat-test-db',
-{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 //kendi kendine veri tabanı yoksa oluşturur senle alakası olmaz
 
@@ -23,22 +22,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index',{
-    photos
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
   });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
 //routes
 app.get('/photos/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id)
-  res.render('photo',{
-    photo
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
   });
 });
 // Foto ekleme sayfası
@@ -46,8 +47,18 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+  if(!fs.existsSync (uploadDir)) { fs.mkdirSync(uploadDir)  }
+  
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
