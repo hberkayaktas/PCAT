5d6e63fd84069d73b91b7aee38b8bd7132a4ed66
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-04-26 15:19:40 +0300

    MVC yapısı Kuruldu
    routelar taşındı ve mvc kurulmuş oldu

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-const path = require('path');
-const fs = require('fs');
-const Photo = require('./models/Photo');
 const fileUpload = require('express-fileupload');
 const methodOverride = require('method-override');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -31,67 +30,19 @@ app.use(
 );
 
 //routes
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+app.get('/', photoController.getAllphotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/edit/:id',photoController.updatePhoto);
+app.delete('/delete-photo/:id', photoController.deletePhoto);
 
-app.get('/photos/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo,
-  });
-});
-//edit photo sayfası
-app.get('/edit-photo/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render('edit-photo', {
-    photo,
-  });
-});
 
-// Foto ekleme sayfası
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-app.post('/photos', async (req, res) => {
-  const uploadDir = 'public/uploads';
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
 
-  let uploadeImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
-  uploadeImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadeImage.name,
-    });
-    res.redirect('/');
-  });
-});
+app.get('/edit-photo/:id', pageController.editPhotoPage);
+app.get('/about', pageController.getAboutPage);
+app.get('/add',pageController.getAddPage);
 
-//foto güncelleme sayfası
-app.put('/photos/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-  res.redirect(`/photos/${req.params.id}`);
-});
 
-app.delete('/delete-photo/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + '/public' + photo.image;
-  fs.unlinkSync(deletedImage);
-  await Photo.findByIdAndRemove(req.params.id);
-  res.redirect('/');
-});
 
 const port = 3000;
 app.listen(port, () => {
